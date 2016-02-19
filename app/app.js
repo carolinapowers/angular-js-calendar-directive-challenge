86ec0cdd42637191bf326b4dea0a79632f8d1e29
Carolina Powers <carolinapoloni@gmail.com>
2016-02-18 23:26:55 -0500

    dates are showing in a calendar frame

--- app/app.js
+++ app/app.js
@@ -1,3 +1,20 @@
-angular.module('calendarDemoApp', []);
 
-// your controller and directive code go here
+angular.module('calendarDemoApp', [])
+    
+    .controller ('AppCtrl', function ($scope) { 
+        $scope.hello ='hello world';
+    
+  
+    })
+    
+    .directive('calendar', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'calendar.directive.html',
+            link: function (scope) {
+                scope.range = CalendarRange.getMonthlyRange(new Date());
+                console.log(scope.range);
+            }
+        }
+    })
+
