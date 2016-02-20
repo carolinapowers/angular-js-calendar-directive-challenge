1f44590178e4e08048b323efb5b222a1b828a3fe
Carolina Powers <carolinapoloni@gmail.com>
2016-02-19 23:55:27 -0500

    added a parent directive for date picker and used 'this' to exppose it to the outside world

--- app/app.js
+++ app/app.js
@@ -2,19 +2,50 @@
 angular.module('calendarDemoApp', [])
     
     .controller ('AppCtrl', function ($scope) { 
-        $scope.hello ='hello world';
     
-  
     })
-    
+
+    .directive ('datepicker', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'datepicker.directive.html',
+            controller: function ($scope, $element, $attrs) {
+                $scope.month = 5;    
+                
+                this.d = new Date();
+                this.d.setMonth ($scope.month);
+               
+                $scope.save = function(model) {
+                    this.d = new Date();
+                    this.d.setMonth (model);
+                    $scope.see = this.d.setMonth ($scope.month); 
+                }
+                  
+            }
+        }
+    })
     .directive('calendar', function () {
         return {
             restrict: 'E',
             templateUrl: 'calendar.directive.html',
-            link: function (scope) {
-                scope.range = CalendarRange.getMonthlyRange(new Date());
+            require:'^?datepicker',
+            link: function (scope, el, attr, datePickerCtrl) {             
+                scope.range = CalendarRange.getMonthlyRange(datePickerCtrl.d);
+               console.log(datePickerCtrl.d);
+                var calendarDate = CalendarRange.prepareDate(new Date());
+                console.log(calendarDate);
+                
+                function changeMonth () {
+                    calendarDate.month = 2;
+                    return calendarDate.month;
+                }
+                
+              
+                //console.log(d)
+                console.log(new Date());
                 console.log(scope.range);
+                
             }
         }
-    })
+    });
 
