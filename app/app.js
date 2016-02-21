c90c48903bfc096cfbc8c67485d83c4da1534c81
Carolina Powers <carolinapoloni@gmail.com>
2016-02-21 15:49:28 -0500

    added class to the days for different month

--- app/app.js
+++ app/app.js
@@ -11,15 +11,33 @@ angular.module('calendarDemoApp', [])
             templateUrl: 'datepicker.directive.html',
             controller: function ($scope, $element, $attrs) {
                 
-                $scope.selectedItem= {}; 
+                var calendarDate = CalendarRange.prepareDate(new Date());
+                $scope.selectedItem= calendarDate.month; 
+                $scope.selectedYear= calendarDate.year;
+                $scope.years = [];
+                $scope.date = date;
                 
-                $scope.months = [{name: 'January', property: 0 },{ name: 'February', property: 1 },{ name: 'March', property: 2 }, {name:'April', property: 3}, {name: 'May', property: 4}, {name: 'June', property: 5}, {name: 'July', property: 6}, {name:'August', property: 7}, {name: 'September', property: 8}, {name: 'October', property: 9}, {name: 'November', property: 10}, {name: 'December', property: 11}];          
+                var date = calendarDate.year;   
+                for (var i = date; i < date + 40; i++) {
+                    $scope.years.push(i);
+                }
+                for (var i = date -1; i > date - 40; i-- ) {
+                    $scope.years.unshift(i);
+                }
+                
+                $scope.getYearRange = function () {
+                    var d = new Date();
+                    d.setYear ($scope.selectedYear); 
+                    this.range = CalendarRange.getMonthlyRange(d);
+                }          
                
                 $scope.getRange = function () {
                     var d = new Date();
-                    d.setMonth ($scope.selectedItem.property); 
+                    d.setMonth ($scope.selectedItem); 
                     this.range = CalendarRange.getMonthlyRange(d);
                 }      
+                
+                $scope.months = [{name: 'January', property: 0 },{ name: 'February', property: 1 },{ name: 'March', property: 2 }, {name:'April', property: 3}, {name: 'May', property: 4}, {name: 'June', property: 5}, {name: 'July', property: 6}, {name:'August', property: 7}, {name: 'September', property: 8}, {name: 'October', property: 9}, {name: 'November', property: 10}, {name: 'December', property: 11}];
             }
         }
     })
@@ -28,15 +46,11 @@ angular.module('calendarDemoApp', [])
             restrict: 'E',
             templateUrl: 'calendar.directive.html',
             require:'^?datepicker',
-            link: function (scope, el, attr, datePickerCtrl) {             
-                scope.range = datePickerCtrl.range ? datePickerCtrl.range : CalendarRange.getMonthlyRange(new Date ());
-                var calendarDate = CalendarRange.prepareDate(new Date());
-                console.log(calendarDate);
+            link: function (scope, el, attr, datePickerCtrl) {               
+                   scope.range = datePickerCtrl.range ? datePickerCtrl.range : CalendarRange.getMonthlyRange(new Date ());
+                console.log(scope.range);
                 
-                function changeMonth () {
-                    calendarDate.month = 2;
-                    return calendarDate.month;
-                }    
+                console.log(scope.range.start.getMonth());
             }
         }
     });
