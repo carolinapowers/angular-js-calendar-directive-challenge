03679dc35733211ea576ea4fd643973f46e6d31d
Carolina Powers <carolinapoloni@gmail.com>
2016-02-21 09:38:15 -0500

    if range is not selected, show new Date ()  date

--- app/app.js
+++ app/app.js
@@ -13,17 +13,12 @@ angular.module('calendarDemoApp', [])
                 
                 $scope.selectedItem= {}; 
                 
-                $scope.months = [{name: 'January', property: 0 },{ name: 'February', property: 1 },{ name: 'March', property: 2 }, {name:'April', property: 3}, {name: 'May', property: 4}, {name: 'June', property: 5}, {name: 'July', property: 6}, {name:'August', property: 7}, {name: 'September', property: 8}, {name: 'October', property: 9}, {name: 'November', property: 10}, {name: 'December', property: 11}];
-                
-              
-//                var d = new Date();
-//                d.setMonth ($scope.selectedItem.property); 
-//                $scope.range = CalendarRange.getMonthlyRange(d);             
+                $scope.months = [{name: 'January', property: 0 },{ name: 'February', property: 1 },{ name: 'March', property: 2 }, {name:'April', property: 3}, {name: 'May', property: 4}, {name: 'June', property: 5}, {name: 'July', property: 6}, {name:'August', property: 7}, {name: 'September', property: 8}, {name: 'October', property: 9}, {name: 'November', property: 10}, {name: 'December', property: 11}];          
                
-                $scope.submit = function () {
-                var d = new Date();
-                d.setMonth ($scope.selectedItem.property); 
-              this.range = CalendarRange.getMonthlyRange(d);
+                $scope.getRange = function () {
+                    var d = new Date();
+                    d.setMonth ($scope.selectedItem.property); 
+                    this.range = CalendarRange.getMonthlyRange(d);
                 }      
             }
         }
@@ -34,22 +29,14 @@ angular.module('calendarDemoApp', [])
             templateUrl: 'calendar.directive.html',
             require:'^?datepicker',
             link: function (scope, el, attr, datePickerCtrl) {             
-                //scope.range = CalendarRange.getMonthlyRange(datePickerCtrl.d);
-               //console.log(datePickerCtrl.d);
-                scope.range = datePickerCtrl.range;
+                scope.range = datePickerCtrl.range ? datePickerCtrl.range : CalendarRange.getMonthlyRange(new Date ());
                 var calendarDate = CalendarRange.prepareDate(new Date());
                 console.log(calendarDate);
                 
                 function changeMonth () {
                     calendarDate.month = 2;
                     return calendarDate.month;
-                }
-                
-              
-                //console.log(d)
-                console.log(new Date());
-                console.log(scope.range);
-                
+                }    
             }
         }
     });
